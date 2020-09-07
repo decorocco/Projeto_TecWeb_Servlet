be58694506ff1d141647b27b4e3f9467a2829540
Giovanni Santos <leviosa@Giovannis-MacBook-Pro.local>
2020-09-07 17:28:28 -0300

    estrutura xml html

---
+++ Forms/src/br/edu/insper/confirm.java
@@ -0,0 +1,50 @@
+package br.edu.insper;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class confirm
+ */
+@WebServlet("/confirm")
+public class confirm extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public confirm() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String htmlResponse = "<html>";
+		htmlResponse += "<body>";
+		htmlResponse +="<h1>Confirmado!</h1></body>";
+		htmlResponse +="</html>";
+
+		PrintWriter out = response.getWriter();
+		
+		out.println(htmlResponse);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
